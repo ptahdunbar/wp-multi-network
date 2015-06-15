91ad95cd58e01b1e9611180f53ac03806e5967b1
Pirate Dunbar <ptah@piratedunbar.com>
2015-06-15 13:51:01 -0400

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Plugin Name: WP Multi-Network
+ * Plugin URI:  http://wordpress.org/extend/plugins/wp-multi-network/
+ * Description: Adds a Network Management UI for super admins in a WordPress Multisite environment
+ * Version:     1.5.1
+ * Author:      johnjamesjacoby, ddean, BrianLayman
+ * Author URI:  http://jjj.me
+ * Tags:        multi, networks, site, network, blog, domain, subdomain, path, multisite, MS
+ * Network:     true
+ * Text Domain: wp-multi-network
+ */
 
 // temp work around until fix:
 // https://github.com/johnjamesjacoby/wp-multi-network/issues/28
